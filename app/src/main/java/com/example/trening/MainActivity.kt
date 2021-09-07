90b33dc74c2add76097094ce72714e748a298c11
Dmitriy <mygzda@gmail.com>
2021-09-07 16:35:36 +0300

    Initial commit

--- app/src/main/java/com/example/trening/MainActivity.kt
+++ app/src/main/java/com/example/trening/MainActivity.kt
@@ -10,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            val countryName = countryNameEditText.text.toString()
+            val covidStatistic = covidStatService.getStat(countryName)
+
+            resultTextView.text = covidStatistic.toString()
+        }
+
+
     }
 }

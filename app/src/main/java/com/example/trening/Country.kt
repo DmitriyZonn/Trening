ce00949e66c2a3fa58010954a96ba9da1879381f
PanevRN <panevrn@gmail.com>
2021-09-07 20:07:40 +0300

    fragments on lesson 07_09_21

--- app/src/main/java/com/example/trening/Country.kt
+++ app/src/main/java/com/example/trening/Country.kt
@@ -1,8 +1,12 @@
 package com.example.trening
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Country(
     val country: String,
     val cases: Long,
     val deaths: Long,
     val recovered: Long
-)
+): Parcelable
